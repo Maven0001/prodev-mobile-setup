c836537c9eb91b932ec5e75de02d45e9c67f4093
Ibrahim Ayinde <mi.wordpressdesign@gmail.com>
2026-01-10 12:18:38 +0100

    Update files

--- prodev-mobile-app-0x03/app/_layout.tsx
+++ prodev-mobile-app-0x03/app/_layout.tsx
@@ -3,7 +3,9 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack
-      screenOptions={{"
+      screenOptions={{
+        headerShown: false,
+      }}
     />
   );
 }
